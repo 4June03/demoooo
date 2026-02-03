9a597eb7b5c3a3f7797679a460a5e7e78d32f301
viettv1 <viettv1@smartosc.com>
2026-02-03 14:49:38 +0700

    feat: change event name

--- components/DemoEventButton.tsx
+++ components/DemoEventButton.tsx
@@ -6,7 +6,7 @@ export const EventButton =()=> {
   return (
     <div>
       <button
-        onClick={() => sendGTMEvent({ event: 'demo_integration_event', value: 'xyz' })}
+        onClick={() => sendGTMEvent({ event: 'button_click', value: 'xyz' })}
         className='p-4 border-2 border-white'
       >
         Send Event
