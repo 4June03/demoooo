b459e253f86f7804166f8e10981ae02c04d5c190
viettv1 <viettv1@smartosc.com>
2026-02-03 17:30:34 +0700

    add ga component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
       <GoogleTagManager gtmId="GTM-PBFC8XW7" />
+      <GoogleAnalytics gaId="G-PHHYFN783M" />
         {children}
       </body>
     </html>
